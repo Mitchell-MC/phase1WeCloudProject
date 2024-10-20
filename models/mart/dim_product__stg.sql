3fa2f836c851352be84f146734062521c7f6b40f
EC2 Default User <ec2-user@ip-172-31-21-53.ap-southeast-1.compute.internal>
2024-10-20 13:36:29 +0000

    Fixed incorrectly added tables

--- models/mart/dim_product__stg.sql
+++
@@ -1,6 +0,0 @@
-select 
-    *,
-    lag(start_date, 1) over(partition by prod_key order by start_date desc) as deactivate_date,
-    iff(deactivate_date is null, true, false) as active_status
-from 
-    {{ ref('stg_product')}}
